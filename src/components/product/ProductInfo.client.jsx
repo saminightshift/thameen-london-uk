695f4aa9399d9632b16b7d132894e81ae8d604ac
Sami Capaldi <sami.wood@tribusdigital.com>
2023-02-07 17:38:30 +0000

    fixed typo on product info component

--- src/components/product/ProductInfo.client.jsx
+++ src/components/product/ProductInfo.client.jsx
@@ -1,6 +1,6 @@
 import Parser from '../../lib/utils';
 
-export function ProductInfo({content, title}) {
+export function ProductInfo({content}) {
   return (
     <>
       <div className="w-full flex flex-col gap-4 border-b-2 border-black pb-8 text-black font-semibold text-left inner-block">
@@ -8,7 +8,7 @@ export function ProductInfo({content, title}) {
       </div>
 
       <div className="w-full flex flex-col gap-4 border-b-2 border-black pb-8">
-        <div className="innder-block text-black text-left prose w-full">
+        <div className="inner-block text-black text-left prose w-full">
           {Parser(content)}
         </div>
       </div>
